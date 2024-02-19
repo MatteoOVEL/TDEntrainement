ba8a7be1a611ed296b4406de29a741435ccc02bb
ovelm <ovelm@D101-S29>
2024-02-19 15:40:27 +0100

    oui

--- TDEntrainementTests/Controllers/ChanteursControllerTests.cs
+++ TDEntrainementTests/Controllers/ChanteursControllerTests.cs
@@ -5,12 +5,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TDEntrainement.Models.EntityFramework;
 
 namespace TDEntrainement.Controllers.Tests
 {
+    
+   
+
+   
+
     [TestClass()]
     public class ChanteursControllerTests
     {
+        private TDEntrainementContext _context;
+        private ChanteursController _controller;
+
+        
+        public ChanteursControllerTests()
+        {
+            var builder = new DbContextOptionsBuilder<TDEntrainementContext>().UseNpgsql("Server=51.83.36.122;port=5432;Database=ovemat;uid=ovemat;SearchPath=tdentrai;password=1gJeFH;");
+            _context = new TDEntrainementContext(builder.Options);
+            _controller = new TDEntrainementContext(_context);
+        }
         [TestMethod()]
         public void ChanteursControllerTest()
         {
